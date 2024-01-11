1256f399231e0c48ddd6e25983cd8995588a8874
Mehidi Hasan <www.mehidi.me@gmail.com>
2024-01-11 15:40:27 +0600

    update login page

--- packages/web-ui-registration/src/components/LoginPoweredBy.tsx
+++ packages/web-ui-registration/src/components/LoginPoweredBy.tsx
@@ -1,16 +1,18 @@
 // import { Box } from '@rocket.chat/fuselage';
 // import { Link } from '@rocket.chat/layout';
-import { useSetting } from '@rocket.chat/ui-contexts';
+// import { useSetting } from '@rocket.chat/ui-contexts';
 import type { ReactElement } from 'react';
 // import { Trans } from 'react-i18next';
 
 export const LoginPoweredBy = (): ReactElement | null => {
-	const hidePoweredBy = useSetting<boolean>('Layout_Login_Hide_Powered_By');
-	if (hidePoweredBy) {
-		return null;
-	}else{
-		return null;
-	}
+	// const hidePoweredBy = useSetting<boolean>('Layout_Login_Hide_Powered_By');
+
+	return null;
+	// if (hidePoweredBy) {
+	// 	return null;
+	// }else{
+	// 	return null;
+	// }
 	// return (
 	// 	<Box mbe={18}>
 	// 		<Trans i18nKey='registration.page.poweredBy'>
